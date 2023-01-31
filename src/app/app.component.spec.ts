7e72e072051765a678fd225b37c9d5470f7b12a2
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-31 13:21:50 -0500

    test ok

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -6,7 +7,9 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
       ],
       declarations: [
         AppComponent
@@ -30,6 +33,24 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testing2 app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('testing2');
   });
+
+  it('Espero que el formulario sea invalido',()=>{
+    const fixture = TestBed.createComponent(AppComponent);
+
+    const app = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    let formulario = app.formLogin;
+    let username = app.formLogin.controls['username'];
+    let password = app.formLogin.controls['password'];
+
+    // username.setValue('elvis.stwar@hotmail.com');
+    // password.setValue('123456789');
+    
+    expect(app.formLogin.invalid).toBeTruthy();
+  })
+
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testing2';
+
+  formLogin!:FormGroup;
+  enviado!:boolean
+
+  constructor(private formBuilder:FormBuilder, private cd:ChangeDetectorRef){}
+
+  ngOnInit(): void {
+      
+    this.formLogin = this.formBuilder.group({
+
+      username:['',
+        [
+          Validators.required,
+          Validators.minLength(5)
+        ]
+      ],
+      password:['',
+        [
+          Validators.required,
+          Validators.minLength(4)
+
+        ]
+      ]
+    })
+    
+  }
+
+  onSubmit(){
+
+    this.enviado=true
+    setTimeout(() => {
+      this.enviado=false
+    }, 1500);
+  }
+
+
+
+
+
 }
